077d21e71be92901fac7467815efbedb26e4f03d
Sonu Tiwari <ankorha@gmail.com>
2020-03-13 20:09:47 +0530

    TDD task

--- index.test.js
+++ index.test.js
@@ -2,7 +2,11 @@ const {
     describe,
     expect,
     it,
-    matchers
+    matchers,
+    itAdd,
+    itSub,
+    itMult,
+    itDiv
 } = require('./index');
 
 let executes = 0;
@@ -13,3 +17,42 @@ describe('describe', ()=>{
         expect(executes).toBe(1);
     })
 });
+const add = (a, b)=>a+b;
+executes = add(1, 2);
+describe('describe: ', ()=>{
+    itAdd("Adder", ()=>{
+        const actual = describe(' ', add);
+        expect(executes).toBe(3);
+    })
+})
+
+function sub(a, b){
+    return a - b;
+}
+executes = sub(2, 1);
+describe('describe: ', ()=>{
+    itAdd("Subtractor", ()=>{
+        const actual = describe(' ', sub);
+        expect(executes).toBe(1);
+    })
+})
+function mult(a, b){
+    return a*b;
+}
+executes = mult(1, 2);
+describe('describe: ', ()=>{
+    itAdd("Multiplier", ()=>{
+        const actual = describe(' ', mult);
+        expect(executes).toBe(2);
+    })
+})
+function div(a, b){
+    return a/b;
+}
+executes = div(1, 2);
+describe('describe: ', ()=>{
+    itAdd("Divider", ()=>{
+        const actual = describe(' ', div);
+        expect(executes).toBe(1/2);
+    })
+})
